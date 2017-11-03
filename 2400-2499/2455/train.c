76e98d853929881a3db16bedd3bf9d67b9a9f626
but212 <wjddlf211@naver.com>
2017-11-03 23:22:10 +0900

    Create train.c

---
+++ 2400-2499/2455/train.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define size 4
+
+const int maxCalc(unsigned int iter, int* in, int* out, int max, int total)
+{
+    short condition = (((short)iter == 0) << 1) | ((short)max < total);
+        if (condition & 2) {
+            return max;
+        } else if (condition & 1) {
+            return maxCalc(iter - 1, in + 1, out + 1, total, total + (*(in + 1) - *(out + 1)));
+        } else {
+            return maxCalc(iter - 1, in + 1, out + 1, max, total + (*(in + 1) - *(out + 1)));
+    }
+}
+
+int main()
+{
+    int in[size], out[size];
+
+    for (int i = 0; i < size; i++)
+        scanf("%d %d", &out[i], &in[i]);
+
+    printf("%d\n", maxCalc(size, in, out, in[0], in[0]));
+
+    return 0;
+}
