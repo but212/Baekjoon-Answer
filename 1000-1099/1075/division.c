fffa57da14016c3df471259db3a93617d36de823
but212 <wjddlf211@naver.com>
2017-11-03 23:05:06 +0900

    Create division.c

---
+++ 1000-1099/1075/division.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+inline int m100(int n) { return n % 100; }
+inline int minCalc(int N, int M)
+{
+  int min = N - m100(N);
+  while (1) {
+    if (min % M == 0)
+      break;
+    min++;
+  }
+  return m100(min);
+}
+
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int min = minCalc(N, M);
+
+    if (min < 10)
+        printf("0%d\n", min);
+    else
+        printf("%d\n", min);
+
+    return 0;
+}
